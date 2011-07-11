130d713f7ed8880d6c69e023076baeacabdf7a73
Andrew McCloud <andrew@amccloud.com>
2011-07-11 00:39:34 -0700

    fixed install issue

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ if version < '2.2.3':
     from distutils.dist import DistributionMetadata
     DistributionMetadata.classifiers = None
     DistributionMetadata.download_url = None
-setup(name = 'django-stripe',
-        version = '0.1',
-        description = 'A user subscription Django app that integrates with Stripe (http://stripe.com/)',
-        author = 'Andrew McCloud',
-        author_email = 'andrew@amccloud.com',
-        url = 'https://github.com/amccloud/django-stripe/',
-        classifiers = [
-              'Programming Language :: Python :: 2',
-           ],
-        packages = ['django-stripe'],
-        package_dir = {'./django_stripe': 'django_stripe'},
-        )
+setup(
+    name = 'django-stripe',
+    version = '0.1',
+    description = 'A user subscription Django app that integrates with Stripe (http://stripe.com/)',
+    author = 'Andrew McCloud',
+    author_email = 'andrew@amccloud.com',
+    url = 'https://github.com/amccloud/django-stripe/',
+    packages = ['django_stripe'],
+    classifiers = [
+          'Programming Language :: Python :: 2',
+    ],
+)
