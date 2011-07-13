eb2c6005331dd8be7054ecb292c38c00790f7f59
Andrew McCloud <andrew@amccloud.com>
2011-07-12 18:33:26 -0700

    allow passing an inital plan as get arg

--- django_stripe/contrib/registration/backends.py
+++ django_stripe/contrib/registration/backends.py
@@ -1,7 +1,14 @@
 from django.contrib.auth.models import Group
+from django.utils.functional import curry
 from registration.backends.simple import SimpleBackend
+from .forms import CreditCardRegistrationForm
 
 class StripeSubscriptionBackend(SimpleBackend):
+    def get_form_class(self, request):
+        return curry(CreditCardRegistrationForm, initial={
+            'plan': request.GET.get('plan'),
+        })
+
     def register(self, request, plan, **kwargs):
         group = Group.objects.get(pk=plan)
         new_user = super(StripeSubscriptionBackend, self).register(request, **kwargs)
--- django_stripe/contrib/registration/urls.py
+++ django_stripe/contrib/registration/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls.defaults import *
-from .forms import CreditCardRegistrationForm
 
 urlpatterns = patterns('registration.views',
     url(r'register/$', 'register', {
         'backend': 'django_stripe.contrib.registration.backends.StripeSubscriptionBackend',
-        'form_class': CreditCardRegistrationForm,
     }, name='registration_register'),
 )
 
