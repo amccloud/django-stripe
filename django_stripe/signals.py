b6aef06d56dda7478b661ac0be57ba630c958c7d
Andrew McCloud <andrew@amccloud.com>
2011-10-18 00:34:11 -0700

    Cleaned up signals. Enforce signal args and added new signals.

--- django_stripe/signals.py
+++ django_stripe/signals.py
@@ -1,6 +1,29 @@
 import django.dispatch
 
-subscription_trial_ending = django.dispatch.Signal(providing_args=['message'])
-recurring_payment_succeeded = django.dispatch.Signal(providing_args=['message'])
-recurring_payment_failed = django.dispatch.Signal(providing_args=['message'])
-subscription_final_payment_attempt_failed = django.dispatch.Signal(providing_args=['message'])
+__all__ = ['recurring_payment_failed', 'invoice_ready', \
+            'recurring_payment_succeeded', 'subscription_trial_ending', \
+            'subscription_final_payment_attempt_failed', 'ping']
+
+recurring_payment_failed = django.dispatch.Signal(providing_args=[
+    'customer', 'livemode', 'event', 'attempt', 'invoice', 'payment',
+])
+
+invoice_ready = django.dispatch.Signal(providing_args=[
+    'customer', 'event', 'invoice',
+])
+
+recurring_payment_succeeded = django.dispatch.Signal(providing_args=[
+    'customer', 'livemode', 'event', 'invoice', 'payment',
+])
+
+subscription_trial_ending = django.dispatch.Signal(providing_args=[
+    'customer', 'event', 'subscription',
+])
+
+subscription_final_payment_attempt_failed = django.dispatch.Signal(providing_args=[
+    'customer', 'event', 'subscription',
+])
+
+ping = django.dispatch.Signal(providing_args=[
+    'event',
+])
--- django_stripe/views.py
+++ django_stripe/views.py
@@ -1,21 +1,30 @@
-from django.http import Http404
+from django.http import Http404, HttpResponse
 from django.utils import simplejson as json
+from django.views.decorators.csrf import csrf_exempt
+
 from .signals import *
 
 EVENT_SIGNAL_MAP = {
-    'subscription_trial_ending': subscription_trial_ending,
-    'recurring_payment_succeeded': recurring_payment_succeeded,
     'recurring_payment_failed': recurring_payment_failed,
+    'invoice_ready': invoice_ready,
+    'recurring_payment_succeeded': recurring_payment_succeeded,
+    'subscription_trial_ending': subscription_trial_ending,
     'subscription_final_payment_attempt_failed': subscription_final_payment_attempt_failed,
+    'ping': ping,
 }
 
+@csrf_exempt
 def webhook_to_signal(request):
     if 'json' not in request.POST:
         raise Http404
+
     message = json.loads(request.POST.get('json'))
-    print message
     event = message.get('event')
+
     if event not in EVENT_SIGNAL_MAP:
         raise Http404
+
     signal = EVENT_SIGNAL_MAP.get(event)
-    signal.send(sender=webhook_to_signal, message=message)
+    signal.send(sender=webhook_to_signal, **message)
+
+    return HttpResponse()
